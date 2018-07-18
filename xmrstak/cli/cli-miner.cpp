8c50cca59219a1003f72d1a7556d976ebb7caf89
nutsnax <35117940+nutsnax@users.noreply.github.com>
2018-07-18 11:47:35 -0400

    Update cli-miner.cpp

--- xmrstak/cli/cli-miner.cpp
+++ xmrstak/cli/cli-miner.cpp
@@ -1063,7 +1063,7 @@ bool check_expert_mode(bool* expertmode, bool* firstTime, bool* startRunning, bo
 	*firstTime = false;
 
 	std::ifstream firstConfig("expert.json");
-	bool expertParamPattern = true;
+	std::regex expertParamPattern(".*expert_mode.*[:].*(true|false).*");
 	std::regex firstRunParamPattern(".*first_run.*[:].*(true|false).*");
 	std::regex startRunningParamPattern(".*start_running.*[:].*(true|false).*");
 	std::smatch base_match;
